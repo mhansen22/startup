27dae95199e654141f35bb4cbc193bdc941a68f9
Mayme Hansen <maymehansen@gmail.com>
2024-03-21 23:57:04 -0600

    js file

---
+++ movies-html/database.js
@@ -0,0 +1,13 @@
+const config = require('./dbConfig.json');
+
+const url = "mongodb+srv://maymehansen:efW1jWeg6vi9ZzVB@cluster0.u4shfjm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(url);
+const db = client.db('rental');
+
+(async function testConnection() {
+  await client.connect();
+  await db.command({ ping: 1 });
+})().catch((ex) => {
+  console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+  process.exit(1);
+});
