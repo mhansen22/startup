bb79bf17fb6f373407fd838434bf0fb7bd2d9f1c
Mayme Hansen <maymehansen@gmail.com>
2024-04-17 15:37:21 -0600

    login stuff on index.js for login page

--- public/index.js
+++ public/index.js
@@ -1,13 +1,45 @@
-function login() {
-    const nameEl = document.querySelector("#Username");
-    localStorage.setItem("Username", nameEl.value);
-    const password = document.querySelector("#password").value;
-    window.location.href = "user.html";
-  }
+//prev login func!
+// function login() {
+//     const nameEl = document.querySelector("#Username");
+//     localStorage.setItem("Username", nameEl.value);
+//     const password = document.querySelector("#password").value;
+//     window.location.href = "user.html";
+//   }
 
-  function createaccount() {
-    const nameEl = document.querySelector("#Username");
-    localStorage.setItem("Username", nameEl.value);
-    const password = document.querySelector("#password").value;
-    window.location.href = "user.html";
-  }
+//   function createaccount() {
+//     const nameEl = document.querySelector("#Username");
+//     localStorage.setItem("Username", nameEl.value);
+//     const password = document.querySelector("#password").value;
+//     window.location.href = "user.html";
+//   }
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const email = document.querySelector("#Username").value;
+  const password = document.querySelector("#password").value;
+  const action = event.submitter.id;
+
+  const url = `/api/auth/${action}`;
+
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email, password })
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (data.id) {
+      localStorage.setItem("Username", email);//set username, local storage
+      window.location.href = "user.html";
+    } else {
+      alert(data.msg);//alert!!!
+    }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+    alert('Failed to perform the operation.');
+  });
+});
